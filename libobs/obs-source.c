5c13aca59da8904fed9000c72eba31cea2c92ed0
Zachary Lund <zachary.lund@streamlabs.com>
2017-08-03 15:43:59 -0700

    Check for sample_rate being zero to prevent crash

--- libobs/obs-source.c
+++ libobs/obs-source.c
@@ -1044,7 +1044,10 @@ void obs_source_video_tick(obs_source_t *source, float seconds)
 static inline uint64_t conv_frames_to_time(const size_t sample_rate,
 		const size_t frames)
 {
-	return (uint64_t)frames * 1000000000ULL / (uint64_t)sample_rate;
+    if (sample_rate)
+	    return (uint64_t)frames * 1000000000ULL / (uint64_t)sample_rate;
+
+    return 0;
 }
 
 static inline size_t conv_time_to_frames(const size_t sample_rate,
