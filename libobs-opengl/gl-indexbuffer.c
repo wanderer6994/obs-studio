a09f8a6fe16b903bbdbbe8bd5e0fb45f09df6fea
Michael Fabian Dirks <info@xaymar.com>
2017-08-04 22:49:58 +0200

    libobs-opengl: Avoid deallocating foreign uncontrolled memory
    This will fix a bunch of unnecessary crashes and give further control over memory back to the calling code.

--- libobs-opengl/gl-indexbuffer.c
+++ libobs-opengl/gl-indexbuffer.c
@@ -26,7 +26,7 @@ static inline bool init_ib(struct gs_index_buffer *ib)
 			ib->size, ib->data, usage);
 
 	if (!ib->dynamic) {
-		bfree(ib->data);
+		//bfree(ib->data);
 		ib->data = NULL;
 	}
 
@@ -65,7 +65,7 @@ void gs_indexbuffer_destroy(gs_indexbuffer_t *ib)
 		if (ib->buffer)
 			gl_delete_buffers(1, &ib->buffer);
 
-		bfree(ib->data);
+		//bfree(ib->data);
 		bfree(ib);
 	}
 }
