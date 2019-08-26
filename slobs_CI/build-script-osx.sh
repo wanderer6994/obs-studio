a193ba55bcb3fc179e50621936b1092df2ff8f03
eddyStreamlabs <eddy.gharbi@streamlabs.com>
2019-08-26 14:42:57 -0700

    Add additional flag to disable the UI

--- slobs_CI/build-script-osx.sh
+++ slobs_CI/build-script-osx.sh
@@ -8,6 +8,7 @@ cmake -DCMAKE_OSX_DEPLOYMENT_TARGET=10.11 \
 -DDepsPath=/tmp/obsdeps \
 -DVLCPath=$PWD/../../vlc-3.0.4 \
 -DENABLE_UI=false \
+-DDISABLE_UI=true \
 -DCOPIED_DEPENDENCIES=false \
 -DCOPY_DEPENDENCIES=true \
 -DENABLE_SCRIPTING=false ..
