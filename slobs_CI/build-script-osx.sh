01d1c5f36767b9239a698dbd5d28160870f9de9c
eddyStreamlabs <eddy.gharbi@streamlabs.com>
2019-08-26 13:52:13 -0700

    Fix build script

--- slobs_CI/build-script-osx.sh
+++ slobs_CI/build-script-osx.sh
@@ -6,12 +6,11 @@ mkdir build
 cd build
 cmake -DENABLE_SPARKLE_UPDATER=ON \
 -DCMAKE_OSX_DEPLOYMENT_TARGET=10.11 \
--DQTDIR=/usr/local/Cellar/qt/5.10.1 \
 -DDepsPath=/tmp/obsdeps \
 -DVLCPath=$PWD/../../vlc-3.0.4 \
--DENABLE_UI=false \
--DCOPIED_DEPENDENCIES=false \
--DCOPY_DEPENDENCIES=true \
--DENABLE_SCRIPTING=false ..
+-DENABLE_UI=OFF \
+-DCOPIED_DEPENDENCIES=OFF \
+-DCOPY_DEPENDENCIES=ON \
+-DENABLE_SCRIPTING=OFF ..
 
 make -j4
