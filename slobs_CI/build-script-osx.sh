8a0a05b32ddefd775fcf413fe4302163c0858f7d
eddyStreamlabs <eddy.gharbi@streamlabs.com>
2019-08-26 15:56:12 -0700

    Deploy Mac

--- slobs_CI/build-script-osx.sh
+++ slobs_CI/build-script-osx.sh
@@ -7,7 +7,7 @@ mkdir build packed_build
 cd build
 cmake -DCMAKE_OSX_DEPLOYMENT_TARGET=10.11 \
 -DDepsPath=/tmp/obsdeps \
--DCMAKE_INSTALL_PREFIX=$PWD/packed_build \
+-DCMAKE_INSTALL_PREFIX=$PWD/../packed_build \
 -DVLCPath=$PWD/../../vlc-3.0.4 \
 -DENABLE_UI=false \
 -DDISABLE_UI=true \
